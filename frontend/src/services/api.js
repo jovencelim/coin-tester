fd915d9defcf6fe3862f58ec9cce224939269b10
Jovence Lim <limjovence@gmail.com>
2026-05-04 14:23:14 +0800

    separated the frontend and backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,9 @@
-export async function sendAudio(file) {
+export async function sendAudio(file, metadata) {
   const formData = new FormData();
+
   formData.append("file", file);
+  formData.append("dropHeight", metadata.dropHeight);
+  formData.append("surface", metadata.surface);
 
   const res = await fetch("http://localhost:8000/analyze", {
     method: "POST",
